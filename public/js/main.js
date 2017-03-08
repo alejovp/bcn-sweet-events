1fae41912982ea3defec52172d0a618da4391599
Alejandro Vazquez <alejovp@gmail.com>
2017-03-08 12:43:23 +0100

    Login/Register section and modals draft added

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,5 @@
-const menu_1 = document.getElementById('menu_1')
-const menu_2 = document.getElementById('menu_2')
+const menu = document.getElementsByClassName('menu')
 const menuNav = document.getElementById('menuNav')
-const main = document.getElementsByTagName('main')[0]
-const footer = document.getElementsByTagName('footer')[0]
 
 function slideMenu (elem) {
   if (elem.classList.contains('slideup')) {
@@ -14,19 +11,10 @@ function slideMenu (elem) {
   }
 }
 
-menu_2.addEventListener('click', function (e) {
-  e.preventDefault()
-  slideMenu(menuNav)
-  menu_2.addEventListener('transitionend', function (e) {
-    main.classList.add('hide')
-    footer.classList.add('hide')
+for (let i = 0; i < menu.length; i++) {
+  menu[i].addEventListener('click', function (e) {
+    e.preventDefault()
+    slideMenu(menuNav)
   })
-})
-
-menu_1.addEventListener('click', function (e) {
-  e.preventDefault()
-  main.classList.remove('hide')
-  footer.classList.remove('hide')
-  slideMenu(menuNav)
-})
+}
 
