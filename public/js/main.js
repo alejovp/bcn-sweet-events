94f2543a33092d365db6e39c6a80b4127f4b6892
Alejandro Vazquez <alejovp@gmail.com>
2017-03-08 09:58:18 +0100

    Home and products templates added

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,32 @@
-$('.menu').on('click', (e) => {
-  e.preventDefault()
-  if ($('.menuNav').hasClass('slideup')) {
-    $('.menuNav').removeClass('slideup').addClass('slidedown')
+const menu_1 = document.getElementById('menu_1')
+const menu_2 = document.getElementById('menu_2')
+const menuNav = document.getElementById('menuNav')
+const main = document.getElementsByTagName('main')[0]
+const footer = document.getElementsByTagName('footer')[0]
+
+function slideMenu (elem) {
+  if (elem.classList.contains('slideup')) {
+    elem.classList.remove('slideup')
+    elem.classList.add('slidedown')
   } else {
-    $('.menuNav').removeClass('slidedown').addClass('slideup')
+    elem.classList.remove('slidedown')
+    elem.classList.add('slideup')
   }
+}
+
+menu_2.addEventListener('click', function (e) {
+  e.preventDefault()
+  slideMenu(menuNav)
+  menu_2.addEventListener('transitionend', function (e) {
+    main.classList.add('hide')
+    footer.classList.add('hide')
+  })
 })
+
+menu_1.addEventListener('click', function (e) {
+  e.preventDefault()
+  main.classList.remove('hide')
+  footer.classList.remove('hide')
+  slideMenu(menuNav)
+})
+
