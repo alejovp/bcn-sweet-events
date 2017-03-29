bccb84cdd04632a8b52d9ba7eb703d48ec9d7a2b
Alejandro Vazquez <alejovp@gmail.com>
2017-03-29 15:11:04 +0200

    Duplicated files removed

--- server/app_routes/admin/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const addProduct = require('./handlers/addProduct.js')
-
-router.post('/', addProduct)
-
-module.exports = router
